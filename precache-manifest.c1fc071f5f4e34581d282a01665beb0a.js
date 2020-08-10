8778a358b9dbfbc367fbe43fecedd31b3ca3f77e
Evgenia Rangaev <alraner93@gmail.com>
2020-08-10 19:56:38 -0400

    Updates

---
+++ precache-manifest.c1fc071f5f4e34581d282a01665beb0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e11ec2b1631ddd1600c44ebc6a41479",
+    "url": "/Sports_Game_React_app/index.html"
+  },
+  {
+    "revision": "ed62532997cabea5237c",
+    "url": "/Sports_Game_React_app/static/css/main.cbc630a4.chunk.css"
+  },
+  {
+    "revision": "83ec443dbaee11d6f020",
+    "url": "/Sports_Game_React_app/static/js/2.fbb68554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sports_Game_React_app/static/js/2.fbb68554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed62532997cabea5237c",
+    "url": "/Sports_Game_React_app/static/js/main.faefcd9b.chunk.js"
+  },
+  {
+    "revision": "ecf471c53a5f48521a38",
+    "url": "/Sports_Game_React_app/static/js/runtime-main.8527e276.js"
+  },
+  {
+    "revision": "f4afced42902a14c1a446832f27a08f2",
+    "url": "/Sports_Game_React_app/static/media/bunny.f4afced4.png"
+  },
+  {
+    "revision": "15752ad88483d7509c9d94b77fe3ffb1",
+    "url": "/Sports_Game_React_app/static/media/hound.15752ad8.png"
+  },
+  {
+    "revision": "b172ee0d881b819f20db857af14192d1",
+    "url": "/Sports_Game_React_app/static/media/raccoon.b172ee0d.png"
+  },
+  {
+    "revision": "872073a1918e0ac80adf92bdfd2fa67b",
+    "url": "/Sports_Game_React_app/static/media/smb_1-up.872073a1.wav"
+  },
+  {
+    "revision": "f0fb24efc66d05e0f7a31ce5490cf288",
+    "url": "/Sports_Game_React_app/static/media/smb_fireball.f0fb24ef.wav"
+  },
+  {
+    "revision": "87db0b851957568987b1b16cab177dbe",
+    "url": "/Sports_Game_React_app/static/media/squirrel.87db0b85.png"
+  }
+]);
